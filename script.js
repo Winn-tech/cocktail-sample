609df78a9ca636ee0530f5db754d0037f992a109
winn-tech <olugodwin99@gmail.com>
2025-06-21 14:18:04 +0100

    my first commit

--- script.js
+++ script.js
@@ -1,215 +1,72 @@
-  const searchByNameUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-const container = document.querySelector('.cocktail-list')
-const input = document.querySelector('#search-input')
-const button = document.getElementById('submit')
-
-button.addEventListener('click', getData)
-
-window.addEventListener('load', getData)
+const input = document.getElementById('search-input');
+const submitButton = document.getElementById("submit")
+const container = document.querySelector('.cocktail-list');
+const cocktailData = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 
 
- function getData(e){
-   e.preventDefault()
-   let inputValue = input.value
-   console.log(inputValue);
+const fetchData = (e)=>{
+  // prevent the default form submission
+  e.preventDefault()
 
-    container.innerHTML = ''
-   // fetch
-   if (inputValue) {
-      fetch(`${searchByNameUrl}${inputValue}`)
-   .then(
-      // resp => console.log(resp)
-      resp =>{
-        if (resp.ok === true) {
-          return resp.json()
-        }
-        else{
-          console.log('The API IS BAD');
-          
-        }
-      }
-   )
-   .then(   
-     data=>{
-      // console.log(data.drinks);
-      let drinks = data.drinks
-      for (let index = 0; index < drinks.length; index++) {
-         // console.log(drinks[index]);
-        let singleDrink = drinks[index]
-        const article = document.createElement('article')
-         article.innerHTML = `
-            <div class="image">
-               <img src='${singleDrink.strDrinkThumb}' />
-            </div>
-            <div class="description">
-               <h4>${singleDrink.strDrink}</h4>
-               <div class="sub">
-                  <p>Category :</p> <p>${singleDrink.strCategory}</p>
-               </div>
-                <a href='details.html'>
-                            <button>
-                              More Details 
-                            </button>
-               </a>
-            </div>
-         `
-         
-        container.appendChild(article)
-      }
-      
+  // get the value from the input field
+  const inputValue = input.value
+
+  container.innerHTML = '';
+  // fetch the cocktail data using the input value
+  fetch(`${cocktailData}${inputValue || 'a' }`)
+
+  .then(
+     (response)=>{
+        return response.json()
      }
-     
-   )
-   input.value = ''
-    
-   }
-   else{
-     inputValue = 'a'
-     fetch(`${searchByNameUrl}${inputValue}`)
-   .then(
-      // resp => console.log(resp)
-      resp =>{
-        if (resp.ok === true) {
-          return resp.json()
-        }
-        else{
-          console.log('The API IS BAD');
-          
-        }
-      }
-   )
-   .then(   
-     data=>{
-      // console.log(data.drinks);
-      let drinks = data.drinks
-      for (let index = 0; index < drinks.length; index++) {
-         // console.log(drinks[index]);
-        let singleDrink = drinks[index]
-        const article = document.createElement('article')
-         article.innerHTML = `
-            <div class="image">
-               <img src='${singleDrink.strDrinkThumb}' />
-            </div>
-            <div class="description">
-               <h4>${singleDrink.strDrink}</h4>
-               <div class="sub">
-                  <p>Category :</p> <p>${singleDrink.strCategory}</p>
-               </div>
-                <a href='details.html'>
-                            <button class='btn'>
-                              More Details 
-                            </button>
-               </a>
-            </div>
-         `
-         
-        container.appendChild(article)
-      }
+  )
+  .then(
+    (data)=>{
+      console.log(data);
       
-     }
-     
-   )
-   input.value = ''
-   }
-   
-}
 
+      const cocktails = data.drinks;
+      for (let index = 0; index < cocktails.length; index++) {
+        const singleDrinks = cocktails[index];
+        console.log(singleDrinks);
 
-const singleDrinkUrl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
-const singleContainer = document.querySelector('#single')
-
-container.addEventListener('click', function(e) {
-  if (e.target.classList.contains('btn')) {
-    e.preventDefault()
-    fetch(`${singleDrinkUrl}11007`)
-    .then(
-      resp =>{
-        if (resp.ok === true) {
-          return resp.json()
-        }
-      }
-    )
-    .then(
-      data => {
-        const result = data.drinks[0]
-        console.log(result);
-
-        const div = document.createElement('div')
-  
-        div.innerHTML = `
-          <div class=image>
-             <img src='${result.strDrinkThumb}'/>
+        // create an article element to display the cocktail
+        const article = document.createElement('article');
+        article.innerHTML = `
+          <div class="image">
+          <img src='${singleDrinks.strDrinkThumb}' alt='${singleDrinks.trDrink}' >
+        </div>
+        <div class="description">
+          <h2>${singleDrinks.strDrink}</h2>
+          <div class="sub">
+            <p>Category :</p> <p>${singleDrinks.strCategory}</p>
           </div>
-          <h1>hello</h1>
+          <div class="sub">
+            <p>Serving glass :</p> <p>${singleDrinks.strGlass}</p>
+          </div>
+        </div>
+         
         `
-        console.log(div);
-        console.log(singleContainer);
-        
-        
-        
+        container.appendChild(article);
+        input.value = '';
         
       }
-    
-    
-    )
-    
-    
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const container = document.querySelector('.cocktail-list')
-// fetch(`${searchByNameUrl}a`)
-//   .then(
-//     // response => console.log(response)
-//        response => response.json()
-//   )
-//   .then (
-//     data =>{
-//       //  console.log(data.drinks)
-//       for (let i = 0; i < data.drinks.length; i++) {
-//         const drink = data.drinks[i];
-//         console.log(drink.strDrink);
-        
-//         const article = document.createElement('div')
-     
-//       article.innerHTML = 
-//       `  <h3>${drink.strDrink}</h3>
-//       `
-//       container.appendChild(article)
-//     }
-
-//     }
+          
       
-    
-//   )
-//   .catch(error => {
-//     console.error('Fetch error:', error);
-//   });
+    }
+  )
+ 
+ 
+  
+  
+}
+submitButton.addEventListener('click', fetchData)
+
+window.addEventListener('load', fetchData)
+
+
+
+
+
+  
   
